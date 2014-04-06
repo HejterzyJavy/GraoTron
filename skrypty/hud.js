e683a097d5faa1a5734e640528cc3dbcfa4a6651
Karol Wozniak <teo.grafik@gmail.com>
2014-04-06 04:55:43 +0200

    zabezpieczenia przejscia miedzy oknami

--- skrypty/hud.js
+++ skrypty/hud.js
@@ -1,3 +1,4 @@
+var oknoKoszar = null;
 $(document).ready(function() {
 	$('.poleSiatki,#pole13').mouseenter(function() {
 		$(this).css("background-color", "#660000");
@@ -8,8 +9,17 @@ $(document).ready(function() {
 	});
 
 	$('#pole10').click(function() {
-		window.open("Budynki/Koszary/index.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=100, left=100, width=1020, height=620");
 
+		oknoKoszar = window.open("Budynki/Koszary/index.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=100, left=100, width=1020, height=620", "koszary", "");
+
+	});
+
+	$("body").mousemove(function() {
+		console.log("dd");
+		if (oknoKoszar && !oknoKoszar.closed) {
+		window.parent.document.getElementById("container").style.visibility="hidden";
+		}
+		if(oknoKoszar && oknoKoszar.closed) window.parent.document.getElementById("container").style.visibility="visible";
 	});
 
 });
