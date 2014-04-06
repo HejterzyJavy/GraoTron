00884ec3d95a39d78ee0aeb9c944cc031406e1e4
Karol Wozniak <teo.grafik@gmail.com>
2014-04-06 04:10:18 +0200

    Podpiete Koszary

---
+++ Budynki/Koszary/skrypty/main.js
@@ -0,0 +1,60 @@
+var tablica = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var wszystkieJednostki = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var info = new Array();
+var jednostkaId = 0;
+
+$(document).ready(function() {
+
+	info = "<p>Miotacz TOPORÓW <strong> " + wszystkieJednostki[0] + "</strong> </p> <p>PIKINIER <strong> " + wszystkieJednostki[1] + "</strong></p> <p>ŁUCZNIK  <strong> " + wszystkieJednostki[2] + "</strong></p> <p>KAWALERIA  <strong> " + wszystkieJednostki[3] + "</strong> </p><p>CIĘZKA KAWALERIA <strong>" + wszystkieJednostki[4] + "</strong></p><p>RYCERZ <strong>" + wszystkieJednostki[5] + "</strong></p><p>KLERYK <strong>" + wszystkieJednostki[6] + "</strong></p>	<p>DOWÓDCA <strong>" + wszystkieJednostki[7] + "</strong></p><p>WILKOR <strong>" + wszystkieJednostki[8] + "</strong></p>";
+	menuLista.innerHTML = info;
+	var index = 0;
+
+	var lista;
+	$('.add').click(function() {
+		id = $(this).parent().parent().attr("id");
+		ilosc = ++tablica[id.charAt(id.length - 1) - 1];
+		$("#" + id + " .goraPola strong").html("[" + ilosc + "]");
+
+	});
+
+	$('.sub').click(function() {
+		id = $(this).parent().parent().attr("id");
+		klasa = $(this).parent().parent().attr("id");
+		if (tablica[id.charAt(id.length - 1)] > 0)
+			ilosc = --tablica[id.charAt(id.length - 1) - 1];
+		$("#" + id + " .goraPola strong").html("[" + ilosc + "]");
+
+	});
+
+	$('.przycisk').click(function() {
+
+		for ( i = 0; i < 9; i++) {
+			wszystkieJednostki[i] += tablica[i];
+			tablica[i] = 0;
+			info = 0;
+			$("#pole" + (i + 1) + " .goraPola strong").html("");
+		}
+
+		info = "<p>Miotacz TOPORÓW <strong> " + wszystkieJednostki[0] + "</strong> </p> <p>PIKINIER <strong> " + wszystkieJednostki[1] + "</strong></p> <p>ŁUCZNIK  <strong> " + wszystkieJednostki[2] + "</strong></p> <p>KAWALERIA  <strong> " + wszystkieJednostki[3] + "</strong> </p><p>CIĘZKA KAWALERIA <strong>" + wszystkieJednostki[4] + "</strong></p><p>RYCERZ <strong>" + wszystkieJednostki[5] + "</strong></p><p>KLERYK <strong>" + wszystkieJednostki[6] + "</strong></p>	<p>DOWÓDCA <strong>" + wszystkieJednostki[7] + "</strong></p><p>WILKOR <strong>" + wszystkieJednostki[8] + "</strong></p>";
+		menuLista.innerHTML = info;
+
+	});
+
+	$('.poleSiatki').mouseenter(function() {
+		id = $(this).attr("id");
+		var index = 0;
+		jednostkaId = id[id.length - 1];
+		$("#obrazek").css("background-image", $("#" + id + " .obrazekJednostki").css("background-image"));
+		
+		$.getJSON('dane.json', function(data) {
+			$("#tytul").html(data.jednostka[jednostkaId-1].nazwa);
+			$("#hp strong").html(data.jednostka[jednostkaId-1].hp);
+			$("#atk strong").html(data.jednostka[jednostkaId-1].atk);
+			$("#def strong").html(data.jednostka[jednostkaId-1].def);
+			$("#luck strong").html(data.jednostka[jednostkaId-1].luck);
+			$("#koszt strong").html(data.jednostka[jednostkaId-1].koszt);
+		});
+
+	});
+
+});
--- skrypty/hud.js
+++ skrypty/hud.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
 	$('.poleSiatki,#pole13').mouseenter(function() {
-		$(this).css("background-color","#660000");
+		$(this).css("background-color", "#660000");
 	});
-	
+
 	$('.poleSiatki,#pole13').mouseleave(function() {
-		$(this).css("background-color","#554236");
+		$(this).css("background-color", "#554236");
 	});
-	
+
+	$('#pole10').click(function() {
+		window.open("Budynki/Koszary/index.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=100, left=100, width=1020, height=620");
+
+	});
 
 });
